114a505c9f3864af725888993d82cc5cc5c9c86c
MeepTortoise <meeptortoise@gmail.com>
2026-02-05 10:12:27 -0500

    Completed task

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import logoImage from "./Images/logo192.png";
+import {Button, Container, Row, Col} from 'react-bootstrap';
 
 function App(): React.JSX.Element {
     return (
@@ -7,6 +9,8 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
+            <h1>Task 3</h1>
+
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
@@ -14,6 +18,25 @@ function App(): React.JSX.Element {
                 Owen Marko
                 Hello World
             </p>
+            <img src={logoImage} alt="React logo"/>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+            <Button onClick={() => {console.log("Hello World!")}}>Log Hello World</Button>
+            <Container>
+                <Row>
+                    <Col>
+                        <div className="colored" style={{backgroundColor: "red"}}>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div className="colored">
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
